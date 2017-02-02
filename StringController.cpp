4e4457316f362757c01ca8976b3eebe7112a9756
mattpow35 <mpow2231@740s-pl-121.local>
2017-02-02 09:25:29 -0700

    used the [] operator .at method . empty method and .substr method

--- StringController.cpp
+++ StringController.cpp
@@ -17,6 +17,14 @@ void StringController :: start()
     
  
     getSize();
+    
+    // use the [] operator on a string.
+    cout << "use the [] operator on a string" << endl;
+    for (int i=0; i < firstName.length(); ++i)
+    {
+        cout << firstName[i] << endl;
+    }
+    
     useAt();
     useEmpty();
     useSubstr();
@@ -25,24 +33,34 @@ void StringController :: start()
 
 void StringController :: getSize()
 {
-    int size = firstName.size();
+    long size = firstName.size();
     cout << "My first name is " << firstName << " and is " << size << " letters long."<< endl;
-    
 }
 
 void StringController :: useAt()
 {
-    
+    //gets the letter at the 4th position.
+    char letter = middleName.at(4);
+    cout << "using .at() the letter at the 4th position in my middle name is " << letter << endl;
 }
 
 void StringController :: useEmpty()
 {
+    bool isEmpty = lastName.empty();
     
+    if(!isEmpty)
+    {
+        cout << "using .empty() my last name is not an empty string" << endl;
+    }
 }
 
 void StringController :: useSubstr()
 {
+    string shortFirstName = firstName.substr(0,4);
+    cout << "using substr the short version of my name is " << shortFirstName << endl;
     
+    string restOfFirstName = firstName.substr(4);
+    cout << "using substr again the rest of my name is " << restOfFirstName << endl;
 }
 
 void StringController :: useCompare()
